7c5394c5677e62584dc299e930aa20745e5a6b49
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2025-02-18 11:11:34 +0100

    add basic navbar #2

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+      <div className="flex items-center gap-2 px-3">Header</div>
+    </header>
+  );
+};
+
+export default Navbar;
