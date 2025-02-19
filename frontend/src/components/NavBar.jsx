974fc1b32ea4f52f12beee2b643925e8bb3c7a3e
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2025-02-19 19:22:46 +0100

    add breadcrumb and link in navbar #2

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,8 +1,63 @@
+import { ShoppingCart } from "lucide-react";
+import { Link, useLocation } from "react-router";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "./ui/breadcrumb";
+import { Fragment } from "react";
+
 const Navbar = () => {
+  const location = useLocation();
+  const pathnames = location.pathname.split("/").filter((x) => x);
   return (
-    <header className="flex h-16 shrink-0 items-center gap-2 border-b">
-      <div className="flex items-center gap-2 px-3">Header</div>
-    </header>
+    <>
+      {/* Header */}
+      <header className="flex h-16 shrink-0 items-center gap-2 border-b px-6">
+        <div className="flex items-center gap-2">
+          <Link to={"/"}>
+            <h1 className="text-xl font-bold">Ecommerce Test</h1>
+          </Link>
+        </div>
+        <div className="flex flex-1 items-center justify-between gap-1 md:justify-end">
+          <ShoppingCart />
+          <div>5</div>
+        </div>
+      </header>
+
+      {/* BreadCrumb  */}
+      <div className="px-6 mt-2">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink>
+                <Link href="/">Home</Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            {pathnames.map((val, idx) => {
+              const path = `/${pathnames.slice(0, idx + 1).join("/")}`;
+              return (
+                <Fragment key={idx}>
+                  <BreadcrumbSeparator />
+                  <BreadcrumbItem>
+                    <BreadcrumbLink>
+                      {path !== "/product" && (
+                        <Link to={path}>{decodeURIComponent(val)}</Link>
+                      )}
+                      {path === "/product" && (
+                        <span>{decodeURIComponent(val)}</span>
+                      )}
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                </Fragment>
+              );
+            })}
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+    </>
   );
 };
 
