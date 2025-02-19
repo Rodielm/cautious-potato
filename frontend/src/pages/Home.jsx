a900dac725f93af8f688ad4772ac91f9dccf688a
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2025-02-19 14:03:09 +0100

    improve home page #2

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,16 @@
-import ProductCard from "@/components/ProductCard";
 import SearchBar from "@/components/SearchBar";
-import data from "../../data.json";
+import ProductList from "@/components/ProductList";
+
 
 const Home = () => {
+  
   return (
     <>
       <div className="flex flex-row-reverse mb-3">
         <SearchBar />
       </div>
       {/* Product ListView */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {data?.map((item) => (
-          <ProductCard key={item.id} product={item} />
-        ))}
-      </div>
+      <ProductList />
     </>
   );
 };
