2dcfb604c8f3f4f1cae6f99e7e660f0eab3ab854
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2025-02-19 14:02:29 +0100

    improve productCard #2

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import { Button } from "./ui/button";
 import {
   Card,
@@ -11,20 +12,26 @@ import {
 const ProductCard = ({ product }) => {
   return (
     <Card>
-      <figure className="p-3">
-        <img className="w-full object-cover rounded-lg" src={product.imgUrl} />
-      </figure>
-      <CardContent>
-        <CardTitle>{product.model}</CardTitle>
-        <CardDescription>{product.brand}</CardDescription>
-        <span className="text-lg font-medium">
-          {product.price ? product.price + "$" : "???"}
-        </span>
-      </CardContent>
+      <Link to={`/product/${product.id}`}>
+        <figure className="relative p-10 w-full">
+          <img
+            className="w-full h-full object-cover rounded-lg"
+            src={product.imgUrl}
+          />
+        </figure>
+
+        <CardContent>
+          <CardTitle>{product.model}</CardTitle>
+          <CardDescription>{product.brand}</CardDescription>
+          <span className="text-lg font-medium">
+            {product.price ? product.price + "$" : "???"}
+          </span>
+        </CardContent>
+      </Link>
       <CardFooter>
         <div className="flex gap-2">
-          <Button>Add Card</Button>
-          <Button>Buy</Button>
+          <Button onClick={() => console.log("Add Card")}>Add Card</Button>
+          <Button onClick={() => console.log("Buy Product")}>Buy</Button>
         </div>
       </CardFooter>
     </Card>
